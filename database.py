80f1e5d03da026f3388ae7e2f47079beb5640954
Mohammad <m@mohammadi.io>
2022-10-13 15:21:19 +0300

    Updating DB credentials

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = 'sqlite+pysqlite:///./mini.db'
 
 # engine = create_engine(url=SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
-engine = create_engine("postgresql://ldolatsggrtrwl:618ec0efdf4a1060862985938f242a7ab01af26c35def1f90ee4917f82c7f072@ec2-54-170-90-26.eu-west-1.compute.amazonaws.com/d4ga7hgqbhkhrl")
+engine = create_engine("postgres://pvbusnkywcezmi:3e618d6e6af05d7244bea222fc636ed750af73135a948721221299ee5c7999be@ec2-34-249-236-155.eu-west-1.compute.amazonaws.com:5432/d1h13q7tq4spr")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
